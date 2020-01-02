d792e82768d3c809a0abc2b36cdd8539fea647fc
Romil <57846370+lehakra@users.noreply.github.com>
2020-01-02 16:55:51 +0000

    batch read

---
+++ ComputerVisionConsoleApp/ComputerVisionConsoleApp/Program.cs
@@ -0,0 +1,86 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ComputerVisionConsoleApp
+{
+    public class Program
+    {
+        // Add your Computer Vision subscription key and endpoint to your environment variables.
+        static string subscriptionKey = "";// Environment.GetEnvironmentVariable("COMPUTER_VISION_SUBSCRIPTION_KEY");
+
+        static string endpoint = "https://.cognitiveservices.azure.com/";//  Environment.GetEnvironmentVariable("COMPUTER_VISION_ENDPOINT");
+
+        // the OCR method path
+        const string ocrRelativePath = "vision/v2.1/ocr";
+        static string uriBase = endpoint + ocrRelativePath;
+
+        const string readApiRelativePath = "vision/v2.0/read/core/asyncBatchAnalyze";
+
+        static string readApiUriBase = endpoint + readApiRelativePath;
+
+
+        static void Main()
+        {
+            // Get the path and filename to process from the user.
+            Console.Write("Enter the path to an image with text you wish to read: ");
+            string imageFilePath = Console.ReadLine();
+
+            if (File.Exists(imageFilePath))
+            {
+                MakeOCRRequest(imageFilePath).Wait();
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid file path");
+            }
+            Console.WriteLine("\nPress Enter to exit...");
+            Console.ReadLine();
+        }
+
+        static async Task MakeOCRRequest(string imageFilePath)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+                string uri = readApiUriBase;
+
+                HttpResponseMessage response;
+
+                byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+                // Add the byte array as an octet stream to the request body.
+                using (ByteArrayContent content = new ByteArrayContent(byteData))
+                {
+                    content.Headers.ContentType =
+                        new MediaTypeHeaderValue("application/octet-stream");
+
+                    response = await client.PostAsync(uri, content);
+                }
+
+                Console.WriteLine("\nResponse:\n\n{0}\n", response.Headers.GetValues("Operation-Location").FirstOrDefault());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n" + e.Message);
+            }
+        }
+
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            using (FileStream fileStream =
+                new FileStream(imageFilePath, FileMode.Open, FileAccess.Read))
+            {
+                BinaryReader binaryReader = new BinaryReader(fileStream);
+                return binaryReader.ReadBytes((int)fileStream.Length);
+            }
+        }
+    }
+}
